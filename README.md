a954358089e9032422d0114178e6347fb43dc96d
Emma Borghult <emma.borghult@duva.se>
2021-03-23 09:34:25 +0100

    Install vue 3 + sanity

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# portfolio
+# Mayixa Portfolio
---
+++ sanity/schemas/blog.js
@@ -0,0 +1,27 @@
+export default {
+    title: "Blog",
+    name: "blog",
+    type: "document",
+    fields: [
+      {
+        title: "Titel",
+        name: "title",
+        type: "string",
+      },
+      {
+        title: "Bild",
+        name: "img",
+        type: "image",
+      },
+      {
+        title: "Datum/tid",
+        name: "time",
+        type: "datetime"
+      },
+      {
+        title: "hej",
+        name: "hej",
+        type: "string",
+      }
+    ]
+  }
