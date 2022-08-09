ee62be20264f69b81f3e1b1b03f2a04574d058d2
Emma Borghult <emma.borghult@duva.se>
2022-08-10 01:01:55 +0200

    Video bg + begin hero + yarn pkgs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,148 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+embed,
+figure,
+figcaption,
+footer,
+header,
+hgroup,
+menu,
+nav,
+output,
+ruby,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+ol,
+ul {
+  list-style: none;
+}
+blockquote,
+q {
+  quotes: none;
+}
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+  content: "";
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
 }
 
-#nav {
-  padding: 30px;
+@import url("https://fonts.googleapis.com/css2?family=La+Belle+Aurore&family=Plaster&family=Yantramanav:wght@300&display=swap");
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+body {
+  overflow-x: hidden;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
+  font-family: "Yantramanav", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #f5eee6;
+  font-size: 16px;
+  background: black;
 }
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="hero">
+      <div class="hero-container">
+        <h1 class="title">mayixa</h1>
+        <h2 class="subtitle">Emma Borghult</h2>
+      </div>
+      <video autoplay muted loop id="bg-video">
+        <source src="../assets/bg-video.mp4" type="video/mp4" />
+      </video>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 };
 </script>
+<style lang="scss" scoped>
+.home {
+  .hero {
+    &-container {
+      background: linear-gradient(
+        0deg,
+        rgba(114, 235, 127, 0.2),
+        rgba(114, 235, 127, 0.2)
+      );
+      position: absolute;
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      .title {
+        font-family: "Plaster", cursive;
+        font-size: 14.437rem;
+        line-height: 0.93;
+        mix-blend-mode: difference;
+        padding: 5rem 5rem 0;
+      }
+      .subtitle {
+        font-family: "La Belle Aurore", cursive;
+        font-size: 6rem;
+        line-height: 0;
+        padding: 0 5rem;
+      }
+    }
+  }
+  #bg-video {
+    height: 100vh;
+    width: 100%;
+    object-fit: cover;
+  }
+}
+</style>
